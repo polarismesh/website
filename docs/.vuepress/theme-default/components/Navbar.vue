61d091ae3cdd0e148e3fe60c3f77a21bb7428387
andrewshan <andrewshan@tencent.com>
2022-06-28 12:14:22 +0800

    fix: 修改文档的路径

--- docs/.vuepress/theme-default/components/Navbar.vue
+++ docs/.vuepress/theme-default/components/Navbar.vue
@@ -23,7 +23,7 @@
         >-->
         <b-nav-item :href="`/project`" class="navlink">项目</b-nav-item>
         <b-nav-item
-          :to="`/${language}/doc/简介/北极星是什么.html#简介`"
+          :to="`/${language}/doc/北极星是什么/简介.html`"
           :class="showLayoutType === 'doc' ? 'navlink active' : 'navlink'"
           >文档</b-nav-item
         >
