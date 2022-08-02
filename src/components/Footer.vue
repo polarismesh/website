0349bbeddf66af6ae32360587e07f833c3d5aebc
tingfuyeh <tingfuyeh@tencent.com>
2022-08-02 12:43:35 +0800

    feat: 增加ICP备案号

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="footer">
     @ 2022 The PolarisMesh Authors | A Tencent Microservice Project
+    <div>
+      <a href="https://beian.miit.gov.cn/#/Integrated/index"
+        >ICP备案号 粤B2-20090059
+      </a>
+    </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style>
+.footer a {
+  color: #999;
+  text-decoration: none;
+}
+</style>
